28b2ea499a5ceb648b45da6fe08fcc37d8f5d6f1
Yoan Roullard <yoan.roullard@gmail.com>
2014-03-09 15:58:37 +0100

    Private dispatch method

--- scripts/controllers/appRouterController.js
+++ scripts/controllers/appRouterController.js
@@ -3,25 +3,54 @@ define([
     'backbone.marionette',
 ], function(Backbone, Marionette) {
 
-    var ApplicationRouterController = Marionette.Controller.extend({
-        initialize: function() {
-            //do some stuff
-        },
-        homeAction: function() {
-            console.log('trigger HOME')
-            this.trigger('HOME_ACTION');
-        },
-        blogAction: function() {
-            console.log('trigger BLOG')
-            this.trigger('BLOG_ACTION');
-        },
-        contactAction: function() {
-            this.trigger('CONTACT_ACTION');
-        },
-        onClose: function() {
-            // put custom code here, to close this controller
-        }
-    });
+    var ApplicationRouterController = Marionette.Controller.extend(
+        (function() {
+
+            /**
+             * Dispatch
+             * @param  {String} module target module
+             * @param  {String} action action name
+             * @param  Array args
+             */
+
+            var dispatch = function(module, action, args) {
+
+                var module = module.toUpperCase(),
+                    action = action.toUpperCase();
+
+                if (args) {
+                    console.log('Router : dispatch ("' + module + ':"' + action + ')', args);
+                    ApplicationRouterController.prototype.trigger(module + ':' + action, args);
+
+                } else {
+                    console.log('Router : dispatch ("' + module + ':"' + action + ')');
+                    ApplicationRouterController.prototype.trigger(module + ':' + action);
+                }
+            };
+            return {
+                initialize: function() {
+                    //do some stuff
+
+                },
+                homeAction: function() {
+                    dispatch('home', 'default');
+                },
+                homeHello: function(name) {
+                    dispatch('home', 'hello', name);
+                },
+                blogAction: function() {
+                    dispatch('blog', 'default');
+                },
+                contactAction: function() {
+                    dispatch('contact', 'default');
+                },
+                onClose: function() {
+                    // put custom code here, to close this controller
+                }
+            }
+
+        })()
+    );
     return ApplicationRouterController;
 
 
